a473e0371b9259ff8915c5ccc8603890f2a5bd43
Farima Torabi <43099035+farimatorabi@users.noreply.github.com>
2021-02-09 16:15:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+async function getCategories(count, offset) {
+    let response = await fetch(`https://jservice.io/api/categories?count=${count}&offset=${offset}`);
+    let data = await response.json();
+    console.log(data)
+    return data
+}
+
+
+function getClueHtml(clueValue) {
+    return `<div class="my-category-clue" style="grid-row-start: ${clueValue / 100 + 1}">$${clueValue}</div>`
+}
+
+
+function getCategoriesHtml(category) {
+    return `<div class="my-category-title">${category.title}</div>
+    ${getClueHtml(100)}
+    ${getClueHtml(200)}
+    ${getClueHtml(300)}
+    ${getClueHtml(400)}  
+    `
+}
+
+
+getCategories(5, 30).then(categories => {
+    console.log(categories)
+    document.body.innerHTML = `<div class="board">
+        ${categories.map(getCategoriesHtml).join('')}
+    </div>`
+})
